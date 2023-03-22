b7d0c38495ca9c896750a43053f4cca5b8af0c2c
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-03-23 07:06:40 +0800

    Update Searchbar.jsx

--- src/Components/Admin/Searchbar.jsx
+++ src/Components/Admin/Searchbar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
-const Searchbar = () => {
-  return (
-    <input type="text" name="text" class="admin-input" placeholder="Search events"/>
-  )
-}
+// const Searchbar = () => {
+//   return (
+//     <input type="text" name="text" class="admin-input" placeholder="Search events"/>
+//   )
+// }
 
-export default Searchbar
+// export default Searchbar
