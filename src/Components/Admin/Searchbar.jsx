0b511f4b4e9e1e764c016228ef36075c90e25363
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-03-23 16:09:12 +0800

    Delete Searchbar.jsx

--- src/Components/Admin/Searchbar.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// const Searchbar = () => {
-//   return (
-//     <input type="text" name="text" class="admin-input" placeholder="Search events"/>
-//   )
-// }
-
-// export default Searchbar
