4ded02efb25f4145af063ef9e1c8daf634a5ffb1
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-03-23 16:10:59 +0800

    Update AdminProfile.jsx

--- src/Components/Admin/AdminProfile.jsx
+++ src/Components/Admin/AdminProfile.jsx
@@ -1,59 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import { FiSettings } from 'react-icons/fi';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import '../../Styles/admin.css';
 
 const Modal = () => {
-    const [showModal, setShowModal] = useState(false);
-    const [admins, setAdmin] = useState([]);
+    const [admins, setAdmins] = useState([]);
+    const [adminUsername, setAdminUsername] = useState('');
 
     useEffect(() => {
-        axios.get('http://localhost:4000/readAdmin')
-            .then(response => {
-                setAdmin(response.data);
+        axios
+            .get('http://localhost:4000/readAdmin')
+            .then((response) => {
+                setAdmins(response.data);
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log(error);
             });
+
+        const storedAdminUsername = localStorage.getItem('adminUsername');
+        if (storedAdminUsername) {
+            setAdminUsername(storedAdminUsername);
+        }
     }, []);
 
-    const openModal = () => {
-        setShowModal(true);
-    };
+    const admin = admins.find((admin) => admin.username === adminUsername);
 
-    const closeModal = () => {
-        setShowModal(false);
-    };
 
     return (
         <>
-            <button className="btn-1" onClick={openModal}>
-                <FiSettings className="ricon" />
-                Settings
-            </button>
-            {showModal ? (
-                <div className="admin-profile">
-                    <div className="adminmodal-content">
-                        <button className="close" onClick={closeModal}>
-                            &times;
-                        </button>
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img className='round-image' src='https://visualpharm.com/assets/314/Admin-595b40b65ba036ed117d36fe.svg' />
-                                    <h3 className='h33'>ADMINISTRATOR</h3>
-                                    {admins.map((admin, index) => (
-                                        <ul key={admin.id}>
-                                            <li>{admin.fullname}</li>
-                                            <li>{admin.username}</li>
-                                            <li>{admin.password}</li>
-                                        </ul>
-                                    ))}
+            <div className="admin-profile">
+                <div className="adminmodal-content">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <img className='round-image' src='https://visualpharm.com/assets/314/Admin-595b40b65ba036ed117d36fe.svg' />
+                                <h1>ADMINISTRATOR</h1>
+                                {admin && (
+                                    <form onSubmit>
+                                        <label htmlFor="fullname">Fullname</label>
+                                        <input
+                                            type="text"
+                                            id="fullname"
+                                            name="fullname"
+                                            value={admin.fullname}
+                                        />
+
+                                        <label htmlFor="username">Username:</label>
+                                        <input
+                                            type="text"
+                                            id="username"
+                                            name="username"
+                                            value={admin.username}
+                                        />
+
+                                        <label htmlFor="password">Password:</label>
+                                        <input
+                                            type="password"
+                                            id="password"
+                                            name="password"
+                                            value={admin.password}
+                                        />
+                                    </form>
+                                )}
+                                <div className='admin-btn'>
+                                    <button className='submit-btn' type='submit'>Update</button>
+                                    <Link to='/Admin'>
+                                        <button className='cancel-btn' type='Cancel'>Cancel</button>
+                                    </Link>
+
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            ) : null}
+            </div>
         </>
     );
 };
