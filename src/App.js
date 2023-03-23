32932399cbb52a44045e842e1d6871e16fefe4e5
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-03-23 16:14:15 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Eventpage from './Views/Eventpage';
 import Home from './Views/Home';
 import Admin from './Views/Admin';
 import EventTablelist from './Components/Admin/EventTablelist';
+import AdminProfile from './Components/Admin/AdminProfile';
 import { Route, Routes } from "react-router-dom";
 
 
@@ -40,6 +41,7 @@ function App() {
       <Route path="/signin" element={<Login/>}/>
       <Route path="/signup" element={<Register/>}/>
       <Route path="/Admin" element={<Admin/>}/>
+      <Route path="/Settings" element={<AdminProfile/>}/>
       <Route path="/Table-list" element={<EventTablelist/>}/>
     </Routes>
     </div>
