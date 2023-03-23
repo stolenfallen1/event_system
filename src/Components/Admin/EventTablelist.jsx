b46a87f8d565cd8f90c82cdd30d55c1a98543c06
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-03-23 16:09:47 +0800

    Update EventTablelist.jsx

--- src/Components/Admin/EventTablelist.jsx
+++ src/Components/Admin/EventTablelist.jsx
@@ -42,18 +42,19 @@ const EventTablelist = () => {
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
     };
-        
+
     const filteredEvents = events.filter((event) => {
         const searchValue = searchTerm.toLowerCase();
         return (
             event.location.toLowerCase().includes(searchValue) ||
             event.status.toLowerCase().includes(searchValue) ||
             event.startDate.includes(searchValue) ||
-            event.endDate.includes(searchValue)
+            event.endDate.includes(searchValue) ||
+            event.title.toLowerCase().includes(searchValue)
         );
     });
-    
-        
+
+
 
     const handleUpdateClick = (event) => {
         setSelectedEvent(event);
@@ -103,7 +104,7 @@ const EventTablelist = () => {
     return (
         <>
             <div className='position'>
-            <input type="text" name="text" class="admin-input" placeholder="Search events" onChange={handleSearch}/>
+                <input type="text" name="text" class="admin-input" placeholder="Search events" onChange={handleSearch} />
                 <table>
                     <thead>
                         <tr>
@@ -120,37 +121,37 @@ const EventTablelist = () => {
                     <tbody>
                         {filteredEvents.map((event, index) => (
                             <tr key={event.id}>
-                            <td>{index + 1}</td>
-                            <td>{event.title}</td>
-                            <td>
-                                {event.content.length > 50
-                                ? `${event.content.slice(0, 50)}...`
-                                : event.content}
-                            </td>
-                            <td>{event.location}</td>
-                            <td>{event.startDate}</td>
-                            <td>{event.endDate}</td>
-                            <td>{event.status}</td>
-                            <td>
-                                <button
-                                className="preview-button"
-                                onClick={() => handlePreview(event)}
-                                >
-                                <AiFillEye />
-                                </button>
-                                <button
-                                className="update-button"
-                                onClick={() => handleUpdateClick(event)}
-                                >
-                                <HiPencilAlt />
-                                </button>
-                                <button
-                                className="delete-button"
-                                onClick={() => handleDeleteClick(event.id)}
-                                >
-                                <FaTrash />
-                                </button>
-                            </td>
+                                <td>{index + 1}</td>
+                                <td>{event.title}</td>
+                                <td>
+                                    {event.content.length > 50
+                                        ? `${event.content.slice(0, 50)}...`
+                                        : event.content}
+                                </td>
+                                <td>{event.location}</td>
+                                <td>{event.startDate}</td>
+                                <td>{event.endDate}</td>
+                                <td>{event.status}</td>
+                                <td>
+                                    <button
+                                        className="preview-button"
+                                        onClick={() => handlePreview(event)}
+                                    >
+                                        <AiFillEye />
+                                    </button>
+                                    <button
+                                        className="update-button"
+                                        onClick={() => handleUpdateClick(event)}
+                                    >
+                                        <HiPencilAlt />
+                                    </button>
+                                    <button
+                                        className="delete-button"
+                                        onClick={() => handleDeleteClick(event.id)}
+                                    >
+                                        <FaTrash />
+                                    </button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
