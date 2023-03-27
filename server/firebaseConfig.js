9b5177351ba5462b58468fd46b02a0d683593175
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-03-27 17:01:59 +0800

    Update firebaseConfig.js

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDLU4cp8yJTvQ0WLt1h50-56MExMBk-PhY",
     authDomain: "event-system-ffe08.firebaseapp.com",
     projectId: "event-system-ffe08",
-    storageBucket: "event-system-ffe08.appspot.com",
+    storageBucket: "gs://event-system-ffe08.appspot.com",
     messagingSenderId: "568462177823",
     appId: "1:568462177823:web:1a130280937da8cff6072a",
     measurementId: "G-XBDMCH8NTH"
